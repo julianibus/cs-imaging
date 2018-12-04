6d446d383d48ab533d9f01bc1c66e6e790d7ea0e
Julian Florian Wienand <J.Wienand@ad.physik.uni-muenchen.de>
2018-12-04 09:25:49 +0100

    add qm class

---
+++ qm.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import math
+
+c = 2.997*10**8
+
+
+class QM:
+    
+    #U(x) = PotPrefactor*I(x)
+    #assuming large detunings & neglegible saturatio
+    def PotPrefactor(laser_wl, transition_gamma, transition_wl):
+        transition_freq = 2*math.pi/transition_wl
+        laser_freq = 2*math.pi/laser_wl
+        return (3*math.pi*c**2)/(2*(transition_freq**3))*(transition_gamma/(transition_freq-laser_freq)-transition_gamma/(transition_freq+laser_freq))
+
+
+#print (QM.PotPrefactor(300*10**-9, 10**6,299*10**-9))
+        
+    #add scattering rate
