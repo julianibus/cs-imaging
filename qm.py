1bd20bb8d0255805a639993f40c1484e5a015d46
Caesium lab <exp-caesium@ad.physik.uni-muenchen.de>
2018-12-04 09:32:43 +0100

    implement scattering rate

--- qm.py
+++ qm.py
@@ -11,8 +11,14 @@ class QM:
     def PotPrefactor(laser_wl, transition_gamma, transition_wl):
         transition_freq = 2*math.pi/transition_wl
         laser_freq = 2*math.pi/laser_wl
-        return (3*math.pi*c**2)/(2*(transition_freq**3))*(transition_gamma/(transition_freq-laser_freq)-transition_gamma/(transition_freq+laser_freq))
+        return -(3*math.pi*c**2)/(2*(transition_freq**3))*(transition_gamma/(transition_freq-laser_freq)-transition_gamma/(transition_freq+laser_freq))
 
+    def ScattPrefactor(laser_wl, transition_gamma, transition_wl):
+        transition_freq = 2*math.pi/transition_wl
+        laser_freq = 2*math.pi/laser_wl
+        return (3*math.pi*c**2)/(2*(transition_freq**3))*(transition_gamma/(transition_freq-laser_freq)-transition_gamma/(transition_freq+laser_freq))**2
+		
+	
 
 #print (QM.PotPrefactor(300*10**-9, 10**6,299*10**-9))
         
