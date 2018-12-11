244baa9bfd6053e5fe84e1a2c8f59b89c791d0a0
Julian Florian Wienand <J.Wienand@ad.physik.uni-muenchen.de>
2018-12-11 09:51:18 +0100

    continue heating rates, fix omega calc.

--- scattpot/qm.py
+++ scattpot/qm.py
@@ -9,13 +9,13 @@ class QM:
     #U(x) = PotPrefactor*I(x)
     #assuming large detunings & neglegible saturatio
     def PotPrefactor(laser_wl, transition_gamma, transition_wl):
-        transition_freq = 2*math.pi/transition_wl
-        laser_freq = 2*math.pi/laser_wl
+        transition_freq = c* 2*math.pi/transition_wl
+        laser_freq = c* 2*math.pi/laser_wl
         return -(3*math.pi*c**2)/(2*(transition_freq**3))*(transition_gamma/(transition_freq-laser_freq)-transition_gamma/(transition_freq+laser_freq))
 
     def ScattPrefactor(laser_wl, transition_gamma, transition_wl):
-        transition_freq = 2*math.pi/transition_wl
-        laser_freq = 2*math.pi/laser_wl
+        transition_freq = c*2*math.pi/transition_wl
+        laser_freq = c*2*math.pi/laser_wl
         return (3*math.pi*c**2)/(2*(transition_freq**3))*(transition_gamma/(transition_freq-laser_freq)-transition_gamma/(transition_freq+laser_freq))**2
 		
         
