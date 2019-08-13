0e817d8c4d856cf0278cde82a8566c74b8c35d63
julianibus <jwienand@aol.com>
2019-08-14 00:35:46 +0200

    finish first version of omega and eta pictures

--- scattpot/lambdickeparameters.py
+++ scattpot/lambdickeparameters.py
@@ -56,14 +56,14 @@ l34 = 1359.2 * 10**-9
 
 #decaywleff = {"6s 2 S 1/2": (l41*g41*ss[0]+l21*g21*ss[1]+l51*g51*ss[4])/(g41*ss[0]+g21*ss[1]+g51*ss[4]), "7p 2 P ?3/2": l21, "7s 2 S 1/2": l23, "6p 2 P ?1/2": (l34*g34*ss[2]+l64*g64*ss[5])/(g34*ss[2]+g64*ss[5]), "6p 2 P ?3/2": (l75*g75*ss[6]+l65*g65*ss[5])/(g75*ss[6]+ g65*ss[5]), "5d 2 D 3/2": l26, "5d 2 D 5/2": l27}
 
-levellabels = ["|1> $6s_{1/2}$", "|2> $7p_{3/2}$", "|3> $7s_{1/2}$", "|4> $6p_{1/2}$", "|5> $6p_{3/2}$", "|6> $5d_{3/2}$", "|7> $5d_{5/2}$"]
+levellabels = ['$|1\\rangle \; \; 6s_{1/2}$', "$|2\\rangle \; \; 7p_{3/2}$", "$|3\\rangle \; \; 7s_{1/2}$", "$|4\\rangle \; \; 6p_{1/2}$", "$|5\\rangle \; \; 6p_{3/2}$", "$|6\\rangle \; \; 5d_{3/2}$", "$|7\\rangle \; \; 5d_{5/2}$"]
 levels = ["6s 2 S 1/2", "7p 2 P ?3/2", "7s 2 S 1/2", "6p 2 P ?1/2", "6p 2 P ?3/2", "5d 2 D 3/2", "5d 2 D 5/2"]
 columns = ['laser wavelength','level', 'pot', 'scatt']
 newdf = pd.DataFrame(columns=columns)
 
-intensities = 10**(np.linspace(6,10, num=30)) #1/m^^2 ##LATTICE INTENSITY
-plt.figure(figsize=(10,7))
-ax.set_prop_cycle(color=['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan'])
+intensities = 10**(np.linspace(5,10, num=30)) #mW/cm^2 ##LATTICE INTENSITY
+plt.figure(figsize=(7,5))
+plt.gca().set_prop_cycle(color=['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan'])
 c3 = 0
 for lambdal in lambdals:
     c3+=1
@@ -82,6 +82,7 @@ for lambdal in lambdals:
         omegas = list()
         lambdickes = list()
         for inten in intensities:
+            inten = inten*10
             omega = math.sqrt(2*math.fabs(pot)*inten/m)*2 *math.pi /(lambdal * 10**(-9))
             omegas.append(omega)
             
@@ -91,14 +92,16 @@ for lambdal in lambdals:
             plt.loglog(intensities, omegas,"-o", marker=",", label=levellabels[c], linestyle = "--") 
             
         
-    plt.title(str(lambdal) + "nm")
-    plt.xlabel("Lattice Beam Intensity (W/m^2)")
-    plt.xlim(10**6,10**10)
-    plt.ylabel("H.O. Frequency (1/s)")
-    plt.legend(loc=4, prop={'size': 10})
-    plt.grid(b=True, which='both', color='0.65', linestyle='-')
+    plt.gca().text(0.04, 0.85, str(lambdal) + " nm", fontsize=10, transform=plt.gca().transAxes)
+    plt.xlabel("$I$ ($mW/cm^{2}$)")
+    plt.xlim(10**5,10**9)
+    plt.ylabel("$\omega$ (1/s)")
+    plt.legend(loc=4, prop={'size': 6})
+    plt.grid(b=True, which='both', color='0.85', linestyle='-')
    # plt.savefig("HO" + str(lambdal) + ".png")
+plt.tight_layout()
 
+plt.savefig("omegas.png")
 plt.show()
 
 l21 = 455.5 * 10**-9 
@@ -112,15 +115,18 @@ l75 = 3491  * 10**-9
 l27 = 1360.6  * 10**-9 
 l26 = 1342.8  * 10**-9 
 l34 = 1359.2 * 10**-9 
-decaylabels = ["|2> -> |1>", "|2> -> |3>", "|3> -> |5>", "|4> -> |1>", "|6> -> |4>", "|5> -> |1>", "|6> -> |5>", "|7> -> |5>", "|2> -> |7>", "|2> -> |6>", "|3> -> |4>"]
+decaylabels = ['$|2\\rangle \\rightarrow |1\\rangle $', '$|2\\rangle \\rightarrow |3\\rangle $', '$|3\\rangle \\rightarrow |5\\rangle $', '$|4\\rangle \\rightarrow |1\\rangle $', '$|6\\rangle \\rightarrow |4\\rangle $', '$|5\\rangle \\rightarrow |1\\rangle $', '$|6\\rangle \\rightarrow |5\\rangle $', '$|7\\rangle \\rightarrow |5\\rangle $', '$|2\\rangle \\rightarrow |7\\rangle $', '$|2\\rangle \\rightarrow |6\\rangle $', '$|3\\rangle \\rightarrow |4\\rangle $']
 decays = [l21, l23, l35, l41, l64, l51, l65, l75, l27, l26, l34]
 levels = ["6s 2 S 1/2", "7p 2 P ?3/2", "7s 2 S 1/2", "6p 2 P ?1/2", "6p 2 P ?3/2", "5d 2 D 3/2", "5d 2 D 5/2"]
 decaylevels = ["6s 2 S 1/2", "7s 2 S 1/2","6p 2 P ?3/2", "6s 2 S 1/2", "6p 2 P ?1/2","6s 2 S 1/2", "6p 2 P ?3/2", "6p 2 P ?3/2", "5d 2 D 5/2","5d 2 D 3/2", "6p 2 P ?1/2"]
 
+plt.figure(figsize=(7,5))
+c3 = 0
 for lambdal in lambdals:
-    plt.clf()
-    fig, ax = plt.subplots()
-    ax.set_prop_cycle(color=['blue', 'orange', 'green', 'red', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan'])
+    c3+=1
+    plt.subplot(2,2,c3)
+    plt.gca().set_prop_cycle(color=["blue", "orange", "green", 'red', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan', "black"])
+
     print(lambdal)
     
     for decayi in range(0,len(decays)):
@@ -136,13 +142,18 @@ for lambdal in lambdals:
             
         plt.loglog(intensities, lambdickes,"-o", marker=",", label=decaylabels[decayi]) ##concert so that xaxis is in mW/cm^2
      
-    plt.title(str(lambdal) + "nm")
-    plt.xlabel("Lattice Beam Intensity (W/m^2)")
-    plt.ylabel("Lamb-Dicke Parameter")
-    plt.xlim(10**6,10**10)
-    plt.legend(loc=3, prop={'size': 10})
-    plt.grid(b=True, which='both', color='0.65', linestyle='-') 
-    plt.savefig("LD" + str(lambdal) + ".png")
+    plt.gca().text(0.72, 0.85, str(lambdal) + " nm", fontsize=10, transform=plt.gca().transAxes)
+    plt.xlabel("$I$ ($mW/cm^{2}$)")
+    plt.xlim(10**5,10**9)
+    plt.ylim(2*10**-2,10**0)
+    plt.ylabel("$\eta$")
+    if c3 == 1:    
+        plt.ylim(10**-2,10**0)
+        plt.legend(prop={'size': 5},loc=8, ncol=4)
+    plt.grid(b=True, which='both', color='0.85', linestyle='-')
+plt.tight_layout()
+
+plt.savefig("etas.png")
 plt.show()        
 newdf.to_csv(output, sep=";", index=False)
 
